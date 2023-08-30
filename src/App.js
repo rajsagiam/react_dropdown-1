265a608548b0b534cbfeba9b869d601f226359f4
Raj Yadav <rajpratapyadav@sagiam.com>
2023-08-30 22:00:16 +0530

    First Commit
    Added to Git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Dropdown from './Dropdown';
 
 function App() {
+  const dropdownItems = ['Yes', 'Probably not'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Should you use a dropdown</h1>
+      <Dropdown items={dropdownItems} />
     </div>
   );
 }
